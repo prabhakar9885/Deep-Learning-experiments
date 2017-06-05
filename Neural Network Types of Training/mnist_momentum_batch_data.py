Corrected the momentum formula in weight update rule

Neural Network Types of Training/mnist_momentum_batch_data.py
@@ -98,7 +98,7 @@ def main():
 	delta_w_kj = 0
 	delta_w_ji = 0
 
-	batch_size = 600
+	batch_size = 6000
 	num_of_batches = int(X.shape[0]/batch_size)
 
 	for i in range(100000):
@@ -109,8 +109,8 @@ def main():
 			Y = sigmoid( net_ji )
 			net_kj = np.dot( Y, w_kj )
 			Z = softmax( net_kj )
-			delta_w_kj = learningRate * Y.T.dot(t-Z) + momentum * delta_w_kj_prev + reg * delta_w_kj
-			delta_w_ji = learningRate * X.T.dot( gradient_sigmoid(Y) * np.dot(t-Z, w_kj.T) ) + momentum * delta_w_ji_prev + reg * delta_w_ji
+			delta_w_kj = learningRate * (Y.T.dot(t-Z) + momentum*delta_w_kj_prev + reg*delta_w_kj)
+			delta_w_ji = learningRate * (X.T.dot(gradient_sigmoid(Y)*np.dot(t-Z, w_kj.T)) + momentum*delta_w_ji_prev + reg*delta_w_ji)
 			w_kj += delta_w_kj
 			w_ji += delta_w_ji
 			delta_w_kj_prev = delta_w_kj

Neural Network Types of Training/mnist_momentum_full_data.py
@@ -103,8 +103,8 @@ def main():
 		Y = sigmoid( net_ji )
 		net_kj = np.dot( Y, w_kj )
 		Z = softmax( net_kj )
-		delta_w_kj = learningRate * Y.T.dot(t-Z) + momentum * delta_w_kj_prev + reg * delta_w_kj
-		delta_w_ji = learningRate * X.T.dot( gradient_sigmoid(Y) * np.dot(t-Z, w_kj.T) ) + momentum * delta_w_ji_prev + reg * delta_w_ji
+		delta_w_kj = learningRate * (Y.T.dot(t-Z) + momentum*delta_w_kj_prev + reg*delta_w_kj)
+		delta_w_ji = learningRate * (X.T.dot( gradient_sigmoid(Y) * np.dot(t-Z, w_kj.T) ) + momentum*delta_w_ji_prev + reg*delta_w_ji)
 		w_kj += delta_w_kj
 		w_ji += delta_w_ji
 		delta_w_kj_prev = delta_w_kj